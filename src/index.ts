51caf6589c5576c89b1eb798c3d7c8ce187fb883
Stonehagen <steinhagen@mailbox.org>
2024-01-15 23:24:28 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config({ path: __dirname + '/../.env' });
+
+const forecastModel: string = process.argv[2] || '';
+if (!forecastModel) {
+  console.log('No forecast model specified.');
+  process.exit(1);
+}
+
+const mongoDB: string = process.env.MONGODB_URI || '';
+if (!mongoDB) {
+  console.log(
+    'No mongoDB connection string. Set MONGODB_URI environment variable.',
+  );
+  process.exit(1);
+}
+
+mongoose.connect(mongoDB);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// updateDatabase(forecastModel).then((res) => db.close());
