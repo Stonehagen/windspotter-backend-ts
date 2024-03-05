d0e3bdcacd8ac949ff6218130d1c829898d33ef9
Stonehagen <steinhagen@mailbox.org>
2024-03-05 16:18:07 +0100

    add rework everything to work with types

--- src/lib/updateForecasts.ts
+++ src/lib/updateForecasts.ts
@@ -5,6 +5,8 @@ import { IForecastModel, IForecastInfo } from '../interfaces/models';
 import { downloadFiles } from './downloadFiles';
 import { getFiles, deleteFiles, sortFilesByValue } from './handleLocalFiles';
 import { splitWgribToNetcdf, convertWgribToNetcdf } from './handleWgrib';
+import { convertNetCdf } from './handleNetCdf';
+import { compileSpotForecasts } from './updateDatabase';
 
 export const updateForecasts = async (forecastName: string) => {
   // get forecast config from database
@@ -27,18 +29,23 @@ export const updateForecasts = async (forecastName: string) => {
   const forecastDBTime: Date = forecastInfo ? forecastInfo.time : new Date(0);
 
   // download files
-  const hasDownloaded = await downloadFiles(forecastDBTime, forecastConfig);
+  const hasDownloaded: boolean = await downloadFiles(
+    forecastDBTime,
+    forecastConfig,
+  );
   if (!hasDownloaded) {
     console.log('could not download files from server');
     return false;
   }
 
   // get downloaded files and convert them to netcdf
-  const gribFiles = await getFiles(`./grib_data_${forecastConfig.name}`);
+  const gribFiles: string[] = await getFiles(
+    `./grib_data_${forecastConfig.name}`,
+  );
 
   // check if the grib files need to be splitted
   if (forecastConfig.split) {
-    const convertedWgrib2Netcdf = await splitWgribToNetcdf(
+    const convertedWgrib2Netcdf: boolean = await splitWgribToNetcdf(
       gribFiles,
       forecastConfig,
     );
@@ -47,7 +54,7 @@ export const updateForecasts = async (forecastName: string) => {
       return false;
     }
   } else {
-    const convertedWgrib2Netcdf = await convertWgribToNetcdf(
+    const convertedWgrib2Netcdf: boolean = await convertWgribToNetcdf(
       gribFiles,
       forecastConfig,
     );
@@ -57,34 +64,29 @@ export const updateForecasts = async (forecastName: string) => {
     }
   }
 
-  const ncFiles = await getFiles(`./grib_data_${forecastConfig.name}`);
-  const sortedNcFiles = forecastConfig.dataValues.map((value) =>
+  const ncFiles: string[] = await getFiles(
+    `./grib_data_${forecastConfig.name}`,
+  );
+  if (!ncFiles || ncFiles.length === 0) {
+    console.log('could not get netcdf files');
+    return false;
+  }
+  const sortedNcFiles: string[][] = forecastConfig.dataValues.map((value) =>
     sortFilesByValue(ncFiles, value, forecastConfig),
   );
-  // await convertAllGfsToJSON(ncFiles, forecastConfig);
+  if (!sortedNcFiles || sortedNcFiles.length === 0) {
+    console.log('could not sort netcdf files');
+    return false;
+  }
 
-  // if (forecastConfigName === 'gfsAWS') {
-  //   console.log('gfsAWS to json');
-  //   await splitWGrib2ToNetcdf(files, forecastConfigName);
-  //   const ncFiles = getFiles(`./grib_data_${forecastConfigName}`);
-  //   await convertAllGfsToJSON(ncFiles, forecastConfigName);
-  // } else {
-  //   const sortedFiles = dataValues.map((value) =>
-  //     sortFilesByValue(files, value, forecastConfigName),
-  //   );
-  //   if (wgrib2) {
-  //     console.log('wgrib2');
-  //     await convertAllWGrib2(sortedFiles, forecastConfigName);
-  //     const ncFiles = getFiles(`./grib_data_${forecastConfigName}`);
-  //     const sortedNcFiles = dataValues.map((value) =>
-  //       sortFilesByValue(ncFiles, value, forecastConfigName),
-  //     );
-  //     await convertAllNetCDFToJSON(sortedNcFiles, forecastConfigName);
-  //   } else {
-  //     console.log('grib to json');
-  //     await convertAllGribToJSON(sortedFiles, forecastConfigName, forecastMap);
-  //   }
-  // }
+  const convertedNetCdf = await convertNetCdf(
+    sortedNcFiles,
+    forecastConfig,
+  );
+  if (!convertedNetCdf) {
+    console.log('could not convert netcdf files');
+    return false;
+  }
 
   const deletedFiles = deleteFiles(`./grib_data_${forecastName}`);
   if (!deletedFiles) {
@@ -92,9 +94,10 @@ export const updateForecasts = async (forecastName: string) => {
     return false;
   }
 
-  console.log('stiching Forecasts');
-  // await compileSpotForecasts();
-  console.log('stiched Forecasts');
-  console.log('Database is up to date');
+  const stichedForecast = await compileSpotForecasts();
+  if (!stichedForecast) {
+    console.log('could not stich forecasts');
+    return false;
+  }
   return true;
 };
