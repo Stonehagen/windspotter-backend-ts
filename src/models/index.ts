5ce34392b8c8988a443a1b0bf41dbdcddf198e11
Stonehagen <steinhagen@mailbox.org>
2024-03-07 21:23:14 +0100

    remove map model, sice it will not be supported any more

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,4 @@
 export { Forecast } from './forecast';
 export { ForecastInfo } from './forecastInfo';
-export { MapForecast } from './mapForecast';
 export { Spot } from './spot';
 export { ForecastModel } from './forecastModel';
