06d197bfc5a0468934f3e1518d8f6501bb4e248c
Stonehagen <steinhagen@mailbox.org>
2024-04-23 22:46:40 +0200

    changed false naming in the modules

--- src/models/mapForecast.ts
+++
@@ -1,9 +0,0 @@
-import { model, Schema } from 'mongoose';
-import { IMapForecast } from '../interfaces/models';
-
-const MapForecastSchema = new Schema<IMapForecast>({
-  forecastInfo: { type: Schema.Types.ObjectId, ref: 'Forecast' },
-  forecastMaps: { type: Object },
-});
-
-export const MapForecast = model('MapForecast', MapForecastSchema);
